b0173f28249b08e85f7bb39313027f8300332f17
sauravtom <stomatrix@gmail.com>
2014-09-23 19:04:25 +0530

    First Blood : App in beta

---
+++ app/src/main/java/com/sauravtom/zenreddit/MyActivity.java
@@ -0,0 +1,450 @@
+package com.sauravtom.zenreddit;
+
+import android.annotation.SuppressLint;
+import android.app.*;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.*;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.*;
+import android.widget.AdapterView.OnItemClickListener;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.sauravtom.zenreddit.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyActivity extends Activity {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mTitle = "";
+    boolean doubleBackToExitPressedOnce;
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        if (!isNetworkAvailable()) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+            alert.setMessage("Cannot find the internet !!");
+
+            alert.setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    finish();
+                    startActivity(getIntent());
+                }
+            });
+
+
+            alert.show();
+        }
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+        setContentView(R.layout.activity_main);
+
+        mTitle = "";
+        getActionBar().setTitle(mTitle);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mDrawerList = (ListView) findViewById(R.id.drawer_list);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.string.drawer_open,
+                R.string.drawer_close) {
+
+            /** Called when drawer is closed */
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+
+            /** Called when a drawer is opened */
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+
+        };
+
+        // Setting DrawerToggle on DrawerLayout
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        // Creating an ArrayAdapter to add items to the listview mDrawerList
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(),
+                R.layout.drawer_list_item, getResources().getStringArray(R.array.menus));
+
+        mDrawerList.setAdapter(adapter);
+
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Setting item click listener for the listview mDrawerList
+        mDrawerList.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+
+                String[] menuItems = getResources().getStringArray(R.array.menus);
+                mTitle = menuItems[position];
+
+                WebViewFragment rFragment = new WebViewFragment();
+
+                Bundle data = new Bundle();
+                data.putInt("position", position);
+                data.putString("url", getUrl(position));
+                rFragment.setArguments(data);
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.replace(R.id.content_frame, rFragment);
+                ft.commit();
+
+                getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+                mDrawerLayout.closeDrawer(mDrawerList);
+
+            }
+        });
+
+        InitFragment("0");
+        AddTabs();
+        // Look up the AdView as a resource and load a request.
+        AdView adView = (AdView)this.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+
+    public void AddTabs(){
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                String url = "http://reddit.com/r/"+tab.getText().toString()+"/.compact";
+                if (tab.getText().toString() == "Home"){
+                    url = "http://reddit.com/.compact";
+                }
+                try{
+                    getActionBar().setTitle("/r/"+tab.getText().toString());
+                }
+                catch (Exception e){
+
+                }
+                InitFragment(url);
+            }
+
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // hide the given tab
+            }
+
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // probably ignore this event
+            }
+        };
+
+        String[] subreddit_arr = loadJSONFromAsset();
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Home")
+                        .setTabListener(tabListener));
+
+        for (int i = 0; i < subreddit_arr.length-1; i++) {
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(subreddit_arr[i])
+                            .setTabListener(tabListener));
+        }
+
+    }
+
+    public String[] loadJSONFromAsset() {
+        String json = "Boo";
+
+        try {
+
+            AssetManager assetManager = getResources().getAssets();
+            InputStream is = assetManager.open("subreddits.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            Log.e("Fdrag","Error reading file");
+            return null;
+        }
+        return json.split(",");
+
+    }
+
+    public void InitFragment(String url){
+        if (url.length() < 2 ) url = "http://www.reddit.com/.compact";
+        WebViewFragment rFragment = new WebViewFragment();
+        Bundle data = new Bundle();
+        data.putInt("position", 1);
+        data.putString("url", url);
+        rFragment.setArguments(data);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.content_frame, rFragment);
+        ft.commit();
+    }
+
+    protected String getUrl(int position) {
+        switch (position) {
+            case 0:
+                GoToDialog();
+                return "";
+            case 1:return "http://www.reddit.com/subreddits/mine.compact";
+            case 2:return "http://www.reddit.com/message/inbox/.compact";
+            case 3:return "http://www.reddit.com/message/moderator/.compact";
+            case 4:
+                SearchDialog();
+                return "";
+            case 5:return "http://www.reddit.com/message/compose/.compact";
+            case 6:return "http://www.reddit.com/submit.compact";
+            case 7:return "http://www.reddit.com/r/random/.compact";
+            case 8:return "http://www.reddit.com/login/.compact";
+            case 9:return "http://sauravtom.com/zenreddit_links/#about";
+            case 10:return "http://sauravtom.com/zenreddit_links/#pro";
+            default:
+                return "";
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+
+        menu.findItem(R.id.action_reload).setVisible(!drawerOpen);
+        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
+        //menu.findItem(R.id.action_download).setVisible(false);
+
+        if(drawerOpen) {
+            getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        }
+        //menu.findItem(R.id.action_share).setShowAsAction(1);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        String current_url = WebViewFragment.webView.getUrl();
+        MenuItem icon_share = menu.findItem(R.id.action_share);
+        MenuItem icon_reload = menu.findItem(R.id.action_reload);
+        MenuItem icon_download = menu.findItem(R.id.action_download);
+
+        icon_download.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                String url = WebViewFragment.webView.getUrl();
+                if (WebViewFragment.typeUrl(url) == "image"){
+                    downloadFile(url);
+                }else{
+                    Toast.makeText(getBaseContext(), "No media found in url.", Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
+
+        icon_reload.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                WebViewFragment.webView.reload();
+                return false;
+            }
+        });
+
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) icon_share.getActionProvider();
+        Intent myIntent = new Intent();
+        myIntent.setAction(Intent.ACTION_SEND);
+        myIntent.putExtra(Intent.EXTRA_TEXT, current_url + " (via @zenreddit)");
+        myIntent.setType("text/plain");
+        mShareActionProvider.setShareIntent(myIntent);
+
+        return true;
+    }
+
+    /*Class that prevents closing tha app on back button press, instead go to the previous page. Double press closes the app.*/
+    @Override
+    public void onBackPressed() {
+
+        if(doubleBackToExitPressedOnce){
+            finish();
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+        String url = WebViewFragment.webView.getUrl();
+
+        if(!url.contains("/comments/") && url.startsWith("http://www.reddit.com") && url != "http://www.reddit.com/.compact"){
+            getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        }
+
+        invalidateOptionsMenu();
+        if(WebViewFragment.webView.canGoBack()){
+            WebViewFragment.webView.goBack();
+        }else{
+            InitFragment("0");
+            //finish();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
+    public void downloadFile(String uRl) {
+        String dirName = "ZenReddit";
+        String[] bits = uRl.split("/");
+        String fileName = bits[bits.length-1];
+
+        File direct = new File(Environment.getExternalStorageDirectory() + "/"+dirName);
+
+        if (!direct.exists()) {
+            direct.mkdirs();
+        }
+
+        DownloadManager mgr = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
+        Uri downloadUri = Uri.parse(uRl);
+        DownloadManager.Request request = new DownloadManager.Request(
+                downloadUri);
+
+        request.setAllowedNetworkTypes(
+                DownloadManager.Request.NETWORK_WIFI
+                        | DownloadManager.Request.NETWORK_MOBILE)
+                .setAllowedOverRoaming(false).setTitle(fileName)
+                .setDescription("Image downloaded via Zen reddit app.")
+                .setDestinationInExternalPublicDir("/"+dirName, fileName);
+
+        Toast.makeText(this, "Downloading Image to ZenReddit folder.", Toast.LENGTH_LONG).show();
+        mgr.enqueue(request);
+
+    }
+
+    public void GoToDialog(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Enter subreddit");
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Go", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String value = input.getText().toString();
+                WebViewFragment.webView.loadUrl("http://www.reddit.com/r/"+value+"/.compact");
+                try{
+                    getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+                    getActionBar().setTitle("/r/" + value);
+                }
+                catch(Exception e){}
+            }
+        });
+
+        alert.setNeutralButton("Random !!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                WebViewFragment.webView.loadUrl("http://www.reddit.com/random/.compact");
+                try{
+                    getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+                    getActionBar().setTitle("/r/random");
+                }
+                catch(Exception e){}
+            }
+        });
+
+        alert.setNegativeButton("Nevermind", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
+    }
+
+    public void SearchDialog(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Enter Keyword");
+
+        final EditText input = new EditText(this);
+        //final EditText subreddit = new EditText(this);
+        alert.setView(input);
+        //alert.setView(subreddit);
+
+        alert.setPositiveButton("Go", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String value = input.getText().toString();
+                //String subreddit = subreddit.getText().toString();
+
+                WebViewFragment.webView.loadUrl("http://www.reddit.com/search.compact?q="+value+"&sort=relevance&t=all");
+                try{
+                    getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+                    getActionBar().setTitle("Search");
+                }
+                catch(Exception e){}
+            }
+        });
+
+        alert.setNegativeButton("Nevermind", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+}
---
+++ app/src/main/java/com/sauravtom/zenreddit/WebViewFragment.java
@@ -0,0 +1,177 @@
+package com.sauravtom.zenreddit;
+
+import android.app.ActionBar;
+import android.app.DownloadManager;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
+import com.sauravtom.zenreddit.R;
+
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WebViewFragment extends Fragment {
+
+    public static WebView webView;
+    public static WebViewClient webViewClient;
+    private ProgressBar spinner;
+    private String[] menus;
+    private int position;
+    private View v;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        position = getArguments().getInt("position");
+        String url = getArguments().getString("url");
+        menus = getResources().getStringArray(R.array.menus);
+
+        v = inflater.inflate(R.layout.fragment_layout, container, false);
+
+        webView = (WebView)v.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        //webView.loadUrl("http://www.reddit.com");
+
+
+        spinner = (ProgressBar)v.findViewById(R.id.progressbar);
+        spinner.setIndeterminate(true);
+        spinner.setVisibility(View.GONE);
+
+        //WebView mWebView = (WebView) findViewById(R.id.webView);
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //Toast.makeText(getBaseContext(), "On create touch", Toast.LENGTH_LONG).show();
+                Log.d("1f1", "1f1 onCreated called");
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_UP:
+                        if (!v.hasFocus()) {
+                            v.requestFocus();
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
+
+        webViewClient = new WebViewClient(){
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                spinner.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, final String url) {
+
+                spinner.setVisibility(View.INVISIBLE);
+                try {
+                    if (typeUrl(url) == "comments/external")
+                        getActivity().getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    Log.e("Fdrag3","getActionBar returned null");
+                }
+
+            }
+
+            //Intercept and change urls to comply with reddit mobile view
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //if(!url.endsWith(".compact") && url.startsWith("http://www.reddit.com/r/")) url += "/.compact";
+                String type = typeUrl(url);
+                try {
+                    if(type == "youtube") {
+                        url = changeYoutubeUrl(url);
+                        getActivity().getActionBar().setTitle("Youtube Player");
+                    }
+                    else if (type == "comments/external") {
+                        getActivity().getActionBar().setTitle("");
+                    }
+                    else if (type == "image") {
+                        //downloadFile(url);
+                    }
+
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    Log.e("Fdrag2","getActionBar returned null");
+                }
+                view.loadUrl(url);
+                return true;
+            }
+        };
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.setWebViewClient(webViewClient);
+        webView.loadUrl(url);
+
+        return v;
+    }
+
+    public static String typeUrl(String url){
+        if(url.contains("youtube.com/") || url.contains("youtu.be")) return "youtube";
+
+        else if(url.startsWith("mailto:")){
+           // getActivity().getActionBar().
+            //v.findViewById(R.id.action_share).performClick();
+        }
+
+        //Subreddits
+        else if(!url.contains("/comments/") && url.startsWith("http://www.reddit.com") && url != "http://www.reddit.com/.compact"){
+            return "subreddit";
+        }
+        // comments or external links
+        else if(url.contains("/comments/") || !url.startsWith("http://www.reddit.com")){
+            if(url.endsWith(".jpg") || url.endsWith(".png") || url.endsWith(".gif")){
+                return "image";
+            }
+            return "comments/external";
+        }
+        //Exceptional case
+        else if(url == "http://www.reddit.com/.compact"){
+            return "home";
+        }
+        return "unknown";
+    }
+
+    public String changeYoutubeUrl(String url){
+        String pattern = "(?<=watch\\?v=|/videos/|embed\\/)[^#\\&\\?]*";
+
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(url);
+
+        if(matcher.find()){
+            String id = matcher.group();
+            String new_url = "http://www.youtube.com/embed/"+ id + "?autoplay=1";
+            //Toast.makeText(getApplicationContext(), new_url,Toast.LENGTH_LONG).show();
+            return new_url;
+        }
+        else{
+            return url;
+        }
+    }
+
+
+
+}
